85b541c55ae2104268d5cc6694a88c6a4052d46c
Kari Barry <kezzsim@gmail.com>
2023-11-30 11:10:31 -0500

    Arguments, how do they work? 🧲

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -8,9 +8,7 @@ from tiled.catalog import from_uri
 from tiled.client import Context, from_context
 from tiled.server.app import build_app
 
-uri = sys.argv[2]
-print(sys.argv[1])
-print(sys.argv[2])
+uri = sys.argv[1]
 catalog = from_uri(uri)
 with Context.from_app(build_app(catalog)) as context:
     client = from_context(context)
