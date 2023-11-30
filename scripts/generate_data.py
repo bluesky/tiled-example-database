797ba60fa03bed915ea30d9c398622ada51aa743
Kari Barry <kezzsim@gmail.com>
2023-11-30 11:07:21 -0500

    Modify generator script to run outside of cont.

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -8,13 +8,15 @@ from tiled.catalog import from_uri
 from tiled.client import Context, from_context
 from tiled.server.app import build_app
 
-uri = sys.argv[1]
+uri = sys.argv[2]
+print(sys.argv[1])
+print(sys.argv[2])
 catalog = from_uri(uri)
 with Context.from_app(build_app(catalog)) as context:
     client = from_context(context)
     # Write data
     for i in range(10000):
-        catalog.write_array(
+        client.write_array(
             [random.randint(1, 1500),random.randint(12, 650),random.randint(3, 2500)],
             metadata={
                 "number": i,
