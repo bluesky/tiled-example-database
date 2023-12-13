7235251edc3d4e704dbf28f58b8e4da95cc78f95
Kari Barry <kezzsim@gmail.com>
2023-12-13 17:18:17 -0500

    Sanity check ⏱️ 003 : Wait

--- scripts/run_tiled.sh
+++ scripts/run_tiled.sh
@@ -4,4 +4,3 @@ set -e
 tiled catalog init TILED_TEST_POSTGRESQL_URI
 pwd
 python scripts/generate_data.py TILED_TEST_POSTGRESQL_URI
-docker exec postgres psql -U postgres -c "SELECT * FROM nodes limit 100;"
