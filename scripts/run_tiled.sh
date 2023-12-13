23bef14d2a779a93f6c8b107e48dcfb2ec4fd848
Kari Barry <kezzsim@gmail.com>
2023-12-13 17:01:04 -0500

    Sanity check 🧘‍♀️ 002 : Are .sh files sequential?

--- scripts/run_tiled.sh
+++ scripts/run_tiled.sh
@@ -4,3 +4,4 @@ set -e
 tiled catalog init TILED_TEST_POSTGRESQL_URI
 pwd
 python scripts/generate_data.py TILED_TEST_POSTGRESQL_URI
+docker exec postgres psql -U postgres -c "SELECT * FROM nodes limit 100;"
