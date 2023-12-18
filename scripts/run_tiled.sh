c96f392b6762b3707a1f2edf32a151177faaa851
Kari Barry <kezzsim@gmail.com>
2023-12-18 15:26:38 -0500

    Add sqlite support 🗄️

--- scripts/run_tiled.sh
+++ scripts/run_tiled.sh
@@ -3,4 +3,8 @@ set -e
 
 tiled catalog init $TILED_TEST_POSTGRESQL_URI
 pwd
+# Run postgres generation script
 python scripts/generate_data.py $TILED_TEST_POSTGRESQL_URI
+# Run SQLite generation script
+tiled catalog init tiled_test_db_sqlite.db
+python scripts/generate_data.py tiled_test_db_sqlite.db
